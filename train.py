e998e8a3ad0ebc6a832b655a8002db946e8d565c
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-02-05 23:29:40 +0100

    Add result writing

--- train.py
+++ train.py
@@ -36,8 +36,8 @@ class Trainer:
         self.L2 = torch.nn.MSELoss().cuda()    # L2 since we are doing multi-variable regression
 
         # training constants
-        self.EPOCHS = 10            # number of epochs
-        self.TRAIN_SIZE = 1000      # number of batches in epoch
+        self.EPOCHS = 2            # number of epochs
+        self.TRAIN_SIZE = 100      # number of batches in epoch
         self.TEST_SIZE = 100        # number of batches in epoch
         self.B = 16                 # batch size
         self.train_set = torch.cat([self.net.gen_batch(self.B).unsqueeze(0) for _ in range(self.TRAIN_SIZE)]).cuda()
@@ -45,14 +45,21 @@ class Trainer:
 
     def train_all(self):
         train_data, test_data = {}, {}
+        final = {}
+
         for name, optimizer in self.optims.items():
             self.net.load_state_dict(self.cache.state_dict())
             self.net.cuda()
             means_train, means_test = self._train(optimizer, name)
             train_data[name] = means_train
             test_data[name] = means_test
+            final[name] = {}
+            final[name]['train'] = means_train[-1]
+            final[name]['test'] = means_test[-1]
+
         self.plot(train_data, test_data)
-        
+        with open(f'results/{self.experiment_name}.txt', 'w+') as f:
+            f.write(str(final))
 
     def _train(self, optimizer, name):
         means_train, means_test = [], []
@@ -106,6 +113,7 @@ class Trainer:
 
 
     def plot(self, train_dict, test_dict):
+        plt.figure()
         colors = ['r', 'g', 'b']
         for color, (name, ys) in zip(colors, train_dict.items()):
             plt.plot(range(self.EPOCHS+1), ys, color, label=name)
@@ -115,23 +123,22 @@ class Trainer:
         plt.xlim(0, self.EPOCHS)
         plt.xticks(range(self.EPOCHS+1))
         plt.title(self.experiment_name)
-        plt.show()
+        #plt.show()
         plt.savefig(f'images/{self.experiment_name}.jpg')
 
 
 def main():
-    t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=False, weight_decay=0)
-    t.train_all()
-    t = Trainer(Linear, 'targets/linear.pth', batch_norm=True,  dropout=False, weight_decay=0)
-    t.train_all()
-    t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=True,  weight_decay=0)
-    t.train_all()
-    t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=False, weight_decay=0.01)
-    t.train_all()
-    t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=False, weight_decay=0.001)
-    t.train_all()
-    t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=False, weight_decay=0.0001)
-    t.train_all()
+    Trainer(Conv, 'targets/conv.pth', batch_norm=False, dropout=False, weight_decay=0).train_all()
+    Trainer(Linear, 'targets/linear.pth', batch_norm=True,  dropout=False, weight_decay=0).train_all()
+    #t.train_all()
+    #t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=True,  weight_decay=0)
+    #t.train_all()
+    #t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=False, weight_decay=0.01)
+    #t.train_all()
+    #t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=False, weight_decay=0.001)
+    #t.train_all()
+    #t = Trainer(Linear, 'targets/linear.pth', batch_norm=False, dropout=False, weight_decay=0.0001)
+    #t.train_all()
 
 if __name__ == '__main__':
     main()
